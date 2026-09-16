4652b1d8825c103f300cdcda9639dac1c21df217
Jie Li <jie.li@aecom.com>
2026-09-16 10:25:44 -0400

    fix styling

--- src/App.tsx
+++ src/App.tsx
@@ -2251,33 +2251,6 @@ function FormulaDisplay({
     };
   }, [comment, formulas, hasFormula]);
 
-  useEffect(() => {
-    const element = displayRef.current;
-    if (!element) return undefined;
-
-    const handleWheel = (event: WheelEvent) => {
-      if (event.deltaY === 0 || element.scrollHeight <= element.clientHeight + 1) return;
-      const canScrollInDirection = event.deltaY < 0
-        ? element.scrollTop > 1
-        : element.scrollTop + element.clientHeight < element.scrollHeight - 1;
-      if (canScrollInDirection) return;
-
-      const tableScroller = element.closest<HTMLElement>('.table-scroll');
-      if (!tableScroller) return;
-      const deltaMultiplier = event.deltaMode === WheelEvent.DOM_DELTA_LINE
-        ? 16
-        : event.deltaMode === WheelEvent.DOM_DELTA_PAGE
-          ? tableScroller.clientHeight
-          : 1;
-      event.preventDefault();
-      event.stopPropagation();
-      tableScroller.scrollTop += event.deltaY * deltaMultiplier;
-    };
-
-    element.addEventListener('wheel', handleWheel, { passive: false });
-    return () => element.removeEventListener('wheel', handleWheel);
-  }, []);
-
   if (!hasFormula) {
     return (
       <div className="formula-display formula-comment-display" ref={displayRef}>
